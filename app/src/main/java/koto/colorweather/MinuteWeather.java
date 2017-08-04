58309ec6783f7aa8efde50c8294d6e8cb20a94da
KotoBloodwell <julianaloaiza27@gmail.com>
2017-08-03 23:31:02 -0500

    .

---
+++ app/src/main/java/koto/colorweather/MinuteWeather.java
@@ -0,0 +1,13 @@
+package koto.colorweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MinuteWeather extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_minute_weather);
+    }
+}
