8e6210d5c576500ddcacf55c0ef9810b4174e85e
KotoBloodwell <julianaloaiza27@gmail.com>
2017-08-04 23:53:31 -0500

    Butter Knife Library

--- app/src/main/java/koto/colorweather/MainActivity.java
+++ app/src/main/java/koto/colorweather/MainActivity.java
@@ -1,7 +1,12 @@
 package koto.colorweather;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+    }
+
+    @OnClick(R.id.txtDailyWeather) void txtDailyWeather() {
+        Intent intentDaily = new Intent(MainActivity.this,DailyWeatherActivity.class);
+        startActivity(intentDaily);
+    }
+
+    @OnClick(R.id.txtHourlyWeather) void txtHourlyWeather() {
+        Intent intentHourly = new Intent(MainActivity.this,HourWeatherActivity.class);
+        startActivity(intentHourly);
+    }
+
+    @OnClick(R.id.txtMinutelyWeather) void txtMinutelyWeather() {
+        Intent intentMinute = new Intent(MainActivity.this,MinuteWeather.class);
+        startActivity(intentMinute);
     }
 }
